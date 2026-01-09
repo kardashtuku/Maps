2a6351c1ff38b91fec654b40089a7aa385e54c06
Kardzer <115513213+kardashtuku@users.noreply.github.com>
2026-01-08 19:59:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,35 +3,28 @@ function ZoomImage()
     if(zoom === false)
     {
         zoom = true
-        document.getElementById("map").style.width = "100%";
+        document.getElementById("map").style.width = "200%";
     }
     else
     {
         zoom = false
-        document.getElementById("map").style.width = "50%";
+        document.getElementById("map").style.width = "100%";
     }
     
 }
 function SwitchImage(direction)
 {
-    if(direction == 0)
-    {
-        i--;
-    }
-    else if(direction == 1)
-    {
-        i++;
-    }
+    i+=direction;
     if(i==-1)
     {
-        i=0
+        i=images.length-1
     }
-    if(i==6)
+    if(i==images.length)
     {
-        i=5
+        i=0
     }
     document.getElementById("map").src = "Images/"+images[i]+".png"
 }
 let i = 0;
-const images = ["Elizabeth","HarrisonGreater","HarrisonGreaterEMPTY","MesaCompleteCounties","MesaEmpty","SouthernIndiana"];
+const images = ["Elizabeth","HarrisonGreater","HarrisonGreaterEMPTY","MesaCompleteCounties","MesaEmpty","SouthernIndiana","Federal","Dipamu","Kard","NimidiaFancy","Pee","RiverPlaceEmpty","StyledByethLabeled"];
 let zoom = false;
